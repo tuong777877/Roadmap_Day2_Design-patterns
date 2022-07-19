5a32615323616180ea36fc0a95e683c5d68cb3c9
Tuong Nguyen <tuong.nguyen@s3corp.com.vn>
2022-07-19 17:43:37 +0700

    first commit

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Demo1
+{
+   interface IClassC
+    {
+        public void ActionC();
+    }
+    interface IClassB
+    {
+        public void ActionB();
+    }
+    class ClassC : IClassC
+    {
+
+        public void ActionC() => Console.WriteLine("Action in ClassC");
+    }
+
+    class ClassB : IClassB
+    {
+        public void ActionB()
+        {
+            Console.WriteLine("Action in ClassB");
+            ClassC c = new ClassC();
+            c.ActionC();
+        }
+    }
+
+    class ClassA
+    {
+        public void ActionA()
+        {
+            Console.WriteLine("Action in ClassA");
+            ClassB b = new ClassB();
+            b.ActionB();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassC objectC = new ClassC();
+            ClassB objectB = new ClassB();
+            ClassA objectA = new ClassA();
+
+            objectA.ActionA();
+
+        }
+    }
+}
